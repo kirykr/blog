ce5d326dab8935c9ceb84a9e154320ed56184701
kiry <kirymeas@gmail.com>
2016-09-18 23:43:27 +0700

    add vilidation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :comments
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                         length: { minimum: 2, maximum: 50 }
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, uniqueness: true, 
+                      length: { minimum: 2, maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
+
 end
