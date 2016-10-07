d0dd2ff99ab835c013be5e9d1d77d7231e9c1cd1
kirykr <kirymeas@gmail.com>
2016-10-07 15:03:02 +0700

    Category done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -22,8 +24,16 @@ class CategoriesController < ApplicationController
     end
   end
   def edit
+    @category = Category.find(params[:id])
   end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated!"
+      redirect_to category_path(@category)
+    end
+  end
   def delete
   end
   private
